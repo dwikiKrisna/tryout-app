d1f5f2641709820a7a3e362ac2a69935b6ebc2ab
dadoseto900 <alfiansm07@gmail.com>
2023-04-20 00:45:44 +0700

    fungsi login sudah bisa

--- app/ProviderWrapper.tsx
+++ app/ProviderWrapper.tsx
@@ -1,14 +1,11 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
 
-export default function ProvidersWrapper({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+function ProvidersWrapper({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       {children} {/* Our entire app -> has access to NextAuth */}
     </SessionProvider>
   );
 }
+export default ProvidersWrapper;
--- app/testpage/page.tsx
+++ app/testpage/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const TestPage = () => {
   const { data: session } = useSession();
   console.log(session);
+  // jika tidak ada session maka akan di redirect ke halaman login
+  if (!session) {
+    redirect("/login");
+  }
 
   return <div>TestPage</div>;
 };
